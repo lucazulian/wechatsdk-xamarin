0c4fbf99bf43528800bf5a76560d4485f21f33b3
Luca Zulian <lucagiuggia@gmail.com>
2015-03-27 19:41:58 +0100

    Android binding SDK

--- Android/androidWeChatBinding/androidWeChatBinding/Properties/AssemblyInfo.cs
+++ Android/androidWeChatBinding/androidWeChatBinding/Properties/AssemblyInfo.cs
@@ -19,6 +19,7 @@ using Android.App;
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
 [assembly: AssemblyVersion ("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
@@ -26,3 +27,6 @@ using Android.App;
 //[assembly: AssemblyDelaySign(false)]
 //[assembly: AssemblyKeyFile("")]
 
+// Add some common permissions, these can be removed if not needed
+[assembly: UsesPermission(Android.Manifest.Permission.Internet)]
+[assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
